e23fc824afb96926246012116f2797a2af71a69c
Ian (The Test Guy) <37348701+ianb120@users.noreply.github.com>
2021-07-17 14:14:17 +0100

    Update listFiltering.py

--- listFiltering.py
+++ listFiltering.py
@@ -4,7 +4,6 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 target_value = 4
 filtered_list = []
 
-
 for number in numbers:
     if number > target_value:
         filtered_list = filtered_list + [number]
