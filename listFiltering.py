9c934748e4e1f5784ea232684f079871f969d9da
Ian (The Test Guy) <37348701+ianb120@users.noreply.github.com>
2021-07-17 14:13:48 +0100

    Create listFiltering.py

---
+++ listFiltering.py
@@ -0,0 +1,12 @@
+# list filtering
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target_value = 4
+filtered_list = []
+
+
+for number in numbers:
+    if number > target_value:
+        filtered_list = filtered_list + [number]
+        
+print(filtered_list)
